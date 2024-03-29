80f8c925f85c1a8f1962f8e9af35b51a87432ac4
FakeMoradin2 <A930132281@my.uvm.edu.mx>
2024-03-29 17:22:09 -0600

    first commit

---
+++ pages/api/db.js
@@ -0,0 +1,29 @@
+import { MongoClient } from "mongodb";
+
+const client = await MongoClient.connect(process.env.MONGODB_URI);
+const db = client.db();
+const recetas = db.collection("Houses");
+export default async function handler(req, res) {
+    const { method, body, query } = req;
+
+    switch (method) {
+        case 'POST':
+            const newProperty = {
+            description: body.description,
+            bathrooms: parseInt(body.bathrooms),
+            bedrooms: parseInt(body.bedrooms),
+            imageUrl: body.imageUrl
+            };
+            try {
+                const answer = await recetas.insertOne(newProperty);
+                return res.status(200).json({ message: "se añadió con éxito" });
+            } catch (error) {
+                return res.status(500).json({ message: "Te falló hermano" });
+            }
+            break;
+        case 'GET':
+            const result = await recetas.find().toArray();
+            return res.status(200).json(result);
+            break;
+        }
+    }  
--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,177 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
 
 export default function Home() {
+  const [properties, setProperties] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [description, setDescription] = useState('');
+  const [bathrooms, setBathrooms] = useState(0);
+  const [bedrooms, setBedrooms] = useState(0);
+  const [imageUrl, setImageUrl] = useState('');
+  const [showForm, setShowForm] = useState(false);
+
+  useEffect(() => {
+    getProperties();
+  }, []);
+
+  const getProperties = async () => {
+    try {
+      const res = await axios.get('/api/db');
+      const data = await res.data;
+      setProperties(data);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+
+  const submitProperty = async () => {
+    setLoading(true);
+    try {
+      if (!description || !bathrooms || !bedrooms || !imageUrl) {
+        toast.error('Completa todos los campos, incluyendo la URL de la imagen');
+        setLoading(false);
+        return;
+      }
+
+      const property = {
+        description: description,
+        bathrooms: bathrooms,
+        bedrooms: bedrooms,
+        imageUrl: imageUrl,
+      };
+
+      await axios.post('/api/db', property, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      getProperties();
+      toast.success('Propiedad agregada exitosamente');
+      resetForm();
+    } catch (error) {
+      console.error(error);
+      toast.error('Error al agregar la propiedad');
+    }
+
+    setLoading(false);
+  };
+
+  const resetForm = () => {
+    setDescription('');
+    setBathrooms(0);
+    setBedrooms(0);
+    setImageUrl('');
+  };
+
+  const toggleForm = () => {
+    setShowForm((prev) => !prev);
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className="bg-gray-200 min-h-screen">
+      <header className="bg-black w-full h-50 relative overflow-hidden">
+        <img 
+          src="/WhatsApp Image 2024-03-23 at 5.33.24 PM.jpeg"                       
+          alt="Casa en Edmonton"
+          className='w-full h-50 object-cover lg:h-80'
         />
-      </div>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center lg:text-left p-4 text-white">
+          <h1 className="text-3xl lg:text-4xl font-bold">Saskatchewan Dr</h1>
+        </div>
+      </header>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <main className="p-4">
+        <Toaster position="bottom-center" />
+        {showForm && (
+          <div className='bg-gray-200 p-4'>
+            <div className='flex flex-col p-4 space-y-4 bg-gray-200'>
+              <label htmlFor="description" className="text-gray-800 font-semibold">Descripción:</label>
+              <input
+                id="description"
+                className='px-4 py-2 text-black border rounded focus:outline-none focus:border-blue-500'
+                type='text'
+                placeholder='Descripción de la propiedad'
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+              <label htmlFor="bathrooms" className="text-gray-800 font-semibold">Baños:</label>
+              <input
+                id="bathrooms"
+                className='px-4 py-2 text-black border rounded focus:outline-none focus:border-blue-500'
+                type='number'
+                placeholder='Número de baños'
+                value={bathrooms}
+                onChange={(e) => setBathrooms(parseInt(e.target.value))}
+              />
+              <label htmlFor="bedrooms" className="text-gray-800 font-semibold">Habitaciones:</label>
+              <input
+                id="bedrooms"
+                className='px-4 py-2 text-black border rounded focus:outline-none focus:border-blue-500'
+                type='number'
+                placeholder='Número de habitaciones'
+                value={bedrooms}
+                onChange={(e) => setBedrooms(parseInt(e.target.value))}
+              />
+              <label htmlFor="imageUrl" className="text-gray-800 font-semibold">URL de la Imagen:</label>
+              <input
+                id="imageUrl"
+                className='px-4 py-2 text-black border rounded focus:outline-none focus:border-blue-500'
+                type='text'
+                placeholder='URL de la imagen'
+                value={imageUrl}
+                onChange={(e) => setImageUrl(e.target.value)}
+              />
+            </div>
+          </div>
+        )}
+        
+        <section className="bg-gray-200 p-4">
+          <div className="flex justify-end p-4 space-x-4 bg-gray-200">
+            <button
+              className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
+              disabled={loading}
+              onClick={submitProperty}
+            >
+              {loading ? 'Detener' : 'Agregar Propiedad'}
+            </button>
+            <button
+              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+              onClick={toggleForm}
+            >
+              {showForm ? 'Ocultar Formulario' : 'Agregar Nueva Propiedad'}
+</button>
+</div>
+</section>
+{loading && <div className="text-center mt-4">Cargando...</div>}
+    
+    <section className="p-4 bg-gray-200 flex flex-wrap">
+      {properties.map((property, i) => (
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-4" key={i}>
+          <div className="bg-white border border-gray-200 p-6 rounded-md flex flex-col items-center mb-4">
+            <img
+              src={property.imageUrl}
+              alt={property.description}
+              className="mb-4 rounded-md object-cover"
+              style={{ width: '100%', height: '250px' }}
+            />
+            <h3 className="text-lg font-semibold text-gray-800 mb-2">
+              {property.description}
+            </h3>
+            <p className="text-gray-600">
+              Baños: {property.bathrooms}, Habitaciones: {property.bedrooms}
+            </p>
+          </div>
+        </div>
+      ))}
+    </section>
+  </main>
+  <footer className="text-center py-4 text-gray-600">
+    © {new Date().getFullYear()} Mi Empresa de Renta
+  </footer>
+</div>
+);
 }
